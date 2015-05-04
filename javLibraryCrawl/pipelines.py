a816c40438b6b47f1a36618017e7e210c1b97fcf
kewei <keweihu@yahoo.com>
2015-05-05 00:20:30 +0200

    FIX::connection not found

--- javLibraryCrawl/pipelines.py
+++ javLibraryCrawl/pipelines.py
@@ -25,14 +25,12 @@ class JavlibrarycrawlPipeline(ImagesPipeline):
 
 
 class MongoDBPipeline(object):
-
-    def __init__(self):
+        
+    def process_item(self, item, spider):
         connection = pymongo.MongoClient(
             settings['MONGODB_SERVER'],
             settings['MONGODB_PORT']
         )
-        
-    def process_item(self, item, spider):
         db = connection[settings['MONGODB_DB']]
         if spider.name == 'actor_spider':
             self.collection = db[settings['MONGODB_COLLECTION']]
